664db9564a18ec1508317881089d269820d38f8e
Cristo RPS <cristobits0101@gmail.com>
2023-10-28 10:00:55 +0100

    Adding activity A2.2 to my repository PHP

--- PHP/MisProgramas7/A2.2-Envio-recepcion-formularios/formulario_cristo.php
+++ PHP/MisProgramas7/A2.2-Envio-recepcion-formularios/formulario_cristo.php
@@ -1,124 +1,248 @@
+<?php
+
+    /**
+     *  - Lee los datos almacenados en el fichero cristo_valores_defecto.txt
+     *  - Almacena los datos del fichero en la memoria ram mediante el array $valores_defecto.
+     */
+    require_once "./controllers/cristo_lee_fichero.php";
+
+    cristo_lee_fichero();
+
+    /**
+     *  - Contiene una función que comprueba si los datos que devuelve el fichero son los enviados anteriormente por el usuario. 
+     */
+    require_once "./validations/cristo_valida_fichero.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Título del documento -->
-    <title>Formularios UT2</title>
-    <!-- Estilos del formulario -->
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        .container {
-            width: 50%;
-            margin: 2% auto;
-            padding: 2%;
-            border: 1px solid darkgray;
-            background-color: lightgray;
-        }
-        h1 {
-            margin: 0;
-            padding: 0;
-            text-align: center;
-        }
-        label {
-            width: 30%;
-            font-weight: bold;
-        }
-        input {
-            width: 70%;
-            float: right;
-        }
-        .disponibilidad {
-            display: flex;
-        }
-        p {
-            margin: 0;
-            padding: 0;
-        }
-        .enviar {
-            width: 20%;
-            float: none;
-            margin: 0 auto;
-        }
-        .checkbox {
-            font-weight: normal !important;
-        }
-    </style>
-</head>
-<body>
-    <!-- Contenedor del formulario -->
-    <div class="container">
-        <!-- Título con el autor del código -->
-        <h1>Cristo Rubén Pérez Suárez</h1>
-        <br/>
-        <!-- Formulario de petición laboral -->
-        <form action="./recibe_datos.php" method="post">
-            <!-- Input de texto -->
-            <div>
-                <label for="name">Nombre:</label>
-                <input type="text" id="name" name="name" value="Cristo Rubén Pérez Suárez">
-            </div>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Título del documento -->
+        <title>Formularios UT2</title>
+
+        <!-- Estilos del formulario -->
+        <link rel="stylesheet" href="./styles/cristo_estilo_formulario.css">
+    </head>
+
+    <body>
+        <!-- Contenedor del formulario -->
+        <div class="container">
+
+            <!-- Título con el autor del código -->
+            <h1>Cristo Rubén Pérez Suárez</h1>
+
             <br/>
-            <div>
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" value="1234">
-            </div>
-            <br/>
-            <div>
-                <label for="repeatPassword">Confirme su contraseña:</label>
-                <input type="password" id="repeatPassword" name="repeatPassword" value="1234">
-            </div>
-            <br/>
-            <!-- Input checkbox -->
-            <fieldset style="font-weight: normal !important;">
-                <legend><b>Selecciona los puestos de interés:</b></legend>
-                <br/>
+
+            <!-- Formulario de petición laboral -->
+            <!-- Añadimos el parámetro enctype="multipart/form-data" para que se puedan enviar archivos y no solo texto -->
+            <!-- El método debe ser de tipo post cuando se envían archivos -->
+            <form 
+                action="./controllers/recibe_datos.php" 
+                method="post" 
+                enctype="multipart/form-data"
+            >
+
+                <!-- Input de texto -->
                 <div>
-                    <input type="checkbox" id="desing" value="UX/UI Design" name="job[]" checked />
-                    <label for="desing" class="checkbox">UX/UI Design</label>
+                    <label for="name">Nombre:</label>
+                    <input 
+                        type="text"
+                        id="name"
+                        name="name"
+                        value="<?php if (response_validation("name", "Cristo Rubén Pérez Suárez", $valores_defecto)) echo $valores_defecto["name"]; ?>">
                 </div>
                 <br/>
                 <div>
-                    <input type="checkbox" id="developer" value="Full-Stack Engineer" name="job[]" />
-                    <label for="developer" class="checkbox">Full-Stack Engineer</label>
+                    <label for="password">Contraseña:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password"
+                        value="<?php if (response_validation("password", "asdf76asd7f6", $valores_defecto)) echo $valores_defecto["password"]; ?>">
                 </div>
                 <br/>
                 <div>
-                    <input type="checkbox" id="devops" value="DevOps" name="job[]" />
-                    <label for="devops" class="checkbox">DevOps</label>
+                    <label for="repeatPassword">Confirme su contraseña:</label>
+                    <input 
+                        type="password" 
+                        id="repeatPassword" 
+                        name="repeatPassword"
+                        value="<?php if (response_validation("repeatPassword", "asdf76asd7f6", $valores_defecto)) echo $valores_defecto["repeatPassword"]; ?>"> 
                 </div>
+
                 <br/>
+
+                <!-- Input checkbox -->
+                <fieldset style="font-weight: normal !important;">
+                    <legend><b>Selecciona los puestos de interés:</b></legend>
+                    <br/>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="desing"
+                            value="UX/UI Design"
+                            name="job[]" 
+                            <?php if (response_validation("desing", "UX/UI Design", $valores_defecto)) echo "checked"; ?>
+                        />
+                        <label for="desing" class="checkbox">UX/UI Design</label>
+                    </div>
+                    <br/>
+                    <div>
+                        <input 
+                            type="checkbox"
+                            id="developer"
+                            value="Full-Stack Engineer"
+                            name="job[]" 
+                            <?php if (response_validation("developer", "Full-Stack Engineer", $valores_defecto)) echo "checked"; ?>
+                        />
+                        <label for="developer" class="checkbox">Full-Stack Engineer</label>
+                    </div>
+                    <br/>
+                    <div>
+                        <input 
+                            type="checkbox" 
+                            id="devops" 
+                            value="DevOps" 
+                            name="job[]"
+                            <?php if (response_validation("devops", "DevOps", $valores_defecto)) echo "checked"; ?>
+                        />
+                        <label for="devops" class="checkbox">DevOps</label>
+                    </div>
+                    <br/>
+                    <div>
+                        <input 
+                            type="checkbox"
+                            id="cloud"
+                            value="Cloud Solutions Architect"
+                            name="job[]"
+                            <?php if (response_validation("cloud", "Cloud Solutions Architect", $valores_defecto)) echo "checked"; ?>
+                        />
+                        <label for="cloud" class="checkbox">Cloud Solutions Architect</label>
+                    </div>
+                    <br/>
+                    <div>
+                        <input 
+                            type="checkbox" 
+                            id="data" 
+                            value="Data Science" 
+                            name="job[]"
+                            <?php if (response_validation("data", "Data Science", $valores_defecto)) echo "checked"; ?>
+                        />
+                        <label for="data" class="checkbox">Data Science</label>
+                    </div>
+                    <br/>
+                </fieldset>
+
+                <br/>
+
+                <!-- Input type radio -->
                 <div>
-                    <input type="checkbox" id="cloud" value="Cloud Solutions Architect" name="job[]" />
-                    <label for="cloud" class="checkbox">Cloud Solutions Architect</label>
+                    <fieldset>
+                        <legend><b>Selecciona un proyecto:</b></legend>
+                            <br/>
+                            <div>
+                                <input 
+                                    type="radio" 
+                                    id="ecommerce" 
+                                    name="proyect" 
+                                    value="ecommerce"
+                                    <?php if (response_validation("ecommerce", "E-commerce", $valores_defecto)) echo "checked"; ?>
+                                />
+                                <label for="ecommerce" class="radio">E-commerce</label>
+                            </div>
+                            <br/>
+                            <div>
+                                <input 
+                                    type="radio" 
+                                    id="i+d" 
+                                    name="proyect" 
+                                    value="i+d"
+                                    <?php if (response_validation("i+d", "I+D", $valores_defecto)) echo "checked"; ?>
+                                />
+                                <label for="i+d" class="radio">I+D</label>
+                            </div>
+                            <br/>
+                    </fieldset>
                 </div>
+
                 <br/>
+
+                <!-- Selector de opciones -->
+                <div class="disponibilidad">
+                    <p><b>Disponibilidad:<span>&nbsp;</b></span></p>
+                    <select 
+                        name="incorporation" 
+                        title="Fecha de incorporación"
+                    >
+                        <option 
+                            value="Inmediata"
+                            <?php if (response_validation("incorporation", "Inmediata", $valores_defecto)) echo "checked"; ?>
+                        >
+                            Inmediata
+                        </option>
+                        <option 
+                            value="15 días" 
+                            <?php if (response_validation("incorporation", "15 días", $valores_defecto)) echo "checked"; ?>
+                        >
+                            15 días
+                        </option>
+                    </select>
+                </div>
+
+                <br/>
+
+                <!-- Input type file para documentos -->
                 <div>
-                    <input type="checkbox" id="data" value="Data Science" name="job[]" />
-                    <label for="data" class="checkbox">Data Science</label>
+                    <label for="fichero1">Selecciona un documento: </label>
+                    <input
+                        id="fichero1" 
+                        type="file" 
+                        name="fichero1"
+                        accept=".txt,.pdf,.docx,.xlsx,.pptx,.odt" 
+                    />
                 </div>
+
                 <br/>
-            </fieldset>
-            <br/>
-            <!-- Selector de opciones -->
-            <div class="disponibilidad">
-                <p><b>Disponibilidad:<span>&nbsp;</b></span></p>
-                <select name="incorporation" title="Fecha de incorporación">
-                    <option value="Inmediata">Inmediata</option>
-                    <option value="15 días" selected>15 días</option>
-                </select>
-            </div>
-            <input type="hidden" id="postId" name="postId" value="Cristo" />
-            <br/>
-            <!-- Botón que envía el formulario a ser validado -->
-            <div style="display: flex;">
-                <input type="submit" value="Enviar" class="enviar">
-            </div>
-        </form>
-    </div>
-</body>
+
+                <!-- Input type file para imágenes -->
+                <div>
+                    <label for="foto">Selecciona una foto: </label>
+                    <input 
+                        type="file" 
+                        id="foto"
+                        name="foto"
+                        accept=".jpg,.gif,.png,.jfif"
+                    />
+                </div>
+
+
+                <input 
+                    type="hidden" 
+                    id="postId" 
+                    name="postId" 
+                    value="Cristo" 
+                />
+
+                <br/><br/><br/>
+
+                <!-- Botón que envía el formulario a ser validado -->
+                <div style="display: flex;">
+                    <input 
+                        type="submit" 
+                        value="Enviar" 
+                        class="enviar"
+                    />
+                </div>
+
+            </form>
+
+        </div>
+        
+    </body>
+
 </html>
