1c22260fc163e6630c87cc4fdccbab956d6ccbb6
Cristo RPS <cristobits0101@gmail.com>
2023-11-11 19:48:32 +0000

    Depurando código

--- PHP/MisProgramas7/exam-ut2/crea_formulario_generico.php
+++ PHP/MisProgramas7/exam-ut2/crea_formulario_generico.php
@@ -66,15 +66,45 @@
 
                             elseif
                             (
-                                str_contains($_value, 'checkbox') ||
-                                str_contains($_value, 'radio')
+                                str_contains($_value, 'checkbox')
                             )
                             {
                                 $_field_data = trim($_text[0]);
                                 $_field_name = trim($_text[1]);
                                 $_field_type = trim($_text[2]);
 
-                                $_field_name .= [];
+                                $_field_name = $_field_name . '[]';                 // Convertimos el name de input en un array.
+
+                                $_value_checkbox = explode(',', $_text[3]);
+
+                                echo $_field_data . '<br/><br/>';
+
+                                foreach($_value_checkbox as $_data)
+                                {
+                                    $_purified_data = trim($_data);
+
+                                    echo "
+                                        <input 
+                                            name='$_field_name'
+                                            type='$_field_type'
+                                            value='$_purified_data' 
+                                        />
+
+                                        $_data
+                                        
+                                        <br/>
+                                        <br/>";
+                                }
+                            }
+
+                            elseif
+                            (
+                                str_contains($_value, 'radio')
+                            )
+                            {
+                                $_field_data = trim($_text[0]);
+                                $_field_name = trim($_text[1]);
+                                $_field_type = trim($_text[2]);
 
                                 $_value_checkbox = explode(',', $_text[3]);
 
