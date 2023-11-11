c1213d4c85ebcc2296b37f4ca5a97914dafe1e45
CRPS <cristobits0101@gmail.com>
2023-11-11 10:39:03 +0000

    Adding exam ut1 and ut2 of dsw

---
+++ PHP/MisProgramas7/exam-ut1/crea_formulario_generico.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cristo Suárez UT1</title>
+</head>
+<body>
+    <?php
+        $_file = fopen('./estructura_formulario.cfg', 'r');
+        $_data = explode(';', fgets($file));
+    ?>
+</body>
+</html>
