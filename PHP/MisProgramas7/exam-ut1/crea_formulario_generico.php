5221a77e46dc07c701411f1656a568a9352fe188
CRPS <cristobits0101@gmail.com>
2023-11-12 10:50:52 +0000

    Refactorizando

--- PHP/MisProgramas7/exam-ut1/crea_formulario_generico.php
+++ PHP/MisProgramas7/exam-ut1/crea_formulario_generico.php
@@ -66,7 +66,39 @@
 
                             elseif
                             (
-                                str_contains($_value, 'checkbox') ||
+                                str_contains($_value, 'checkbox')
+                            )
+                            {
+                                $_field_data = trim($_text[0]);
+                                $_field_name = trim($_text[1]);
+                                $_field_type = trim($_text[2]);
+
+                                $_field_name = $_field_name . '[]';                 // Convertimos el name de input en un array.
+
+                                $_value_checkbox = explode(',', $_text[3]);
+
+                                echo $_field_data . '<br/><br/>';
+
+                                foreach($_value_checkbox as $_data)
+                                {
+                                    $_purified_data = trim($_data);
+
+                                    echo "
+                                        <input 
+                                            name='$_field_name'
+                                            type='$_field_type'
+                                            value='$_purified_data' 
+                                        />
+
+                                        $_data
+                                        
+                                        <br/>
+                                        <br/>";
+                                }
+                            }
+
+                            elseif
+                            (
                                 str_contains($_value, 'radio')
                             )
                             {
@@ -80,11 +112,13 @@
 
                                 foreach($_value_checkbox as $_data)
                                 {
+                                    $_purified_data = trim($_data);
+
                                     echo "
                                         <input 
                                             name='$_field_name'
                                             type='$_field_type'
-                                            value='$_data' 
+                                            value='$_purified_data' 
                                         />
 
                                         $_data
@@ -94,7 +128,6 @@
                                 }
                             }
                         }
-
                     }
 
                     echo "
--- PHP/MisProgramas7/exam-ut2/procesa_formulario_generico.php
+++ PHP/MisProgramas7/exam-ut2/procesa_formulario_generico.php
@@ -1,27 +1,70 @@
 <?php
 
-// Directorio donde almacenaremos los ficheros (debe estar creada previamente)
-$directorio = "./ficheros/";
-$datos_formulario = "";
+    $directory = "./files/";                                            // Carpeta que va a almacenar los ficheros.
 
-// Recorremos todos los campos
-foreach ($_REQUEST as $campo => $valor) {
-    $datos_formulario .= "$campo - $valor\n";
-}
-
-// Recorremos todos los ficheros
-foreach ($_FILES as $campo => $archivo) {
-    $nombre_archivo = $archivo["name"];
+    $data = "";                                                         // Variable que recoge los datos del formulario.
     
-    // Registramos el nombre del campo tipo file y su ubicación
-    $datos_formulario .= "$campo - $directorio$nombre_archivo\n";
-    // Movemos el fichero
-    move_uploaded_file($archivo["tmp_name"], $directorio.$nombre_archivo);
-}
+    foreach ($_REQUEST as $field => $value)                             // Recorremos los datos del formulario.
+    {
+        if (isset($value) && !empty($value) && !is_array($field))       // Recogemos los campos que pasen las validaciones.
+            $data .= "$field: $value\n";
 
-$datos_formulario .= "\n";
+        elseif (isset($value) && !empty($value) && is_array($field))    // Si una de las claves es un array cambiamos el formato de concatenación.
+        {
+            $data .= "$field: " . implode(', ', $value) . "\n";         // Convierte el array a una cadena y lo añade al archivo.
+        }
 
-// Almacenamos toda la información en el fichero
-file_put_contents("formulario_recibido.txt", $datos_formulario, FILE_APPEND);
+        else                                                            // Alertamos al usuario de los erróneos en los campos.
+            echo "
+                <script>
+                    alert('Error en campo $field no almacenado.')
+                </script>";
+    }
+
+    foreach ($_FILES as $key => $file)                                  // Recorremos los datos de los ficheros.
+    {
+        $allowed_types = array("image/jpeg", "image/png", "image/gif"); // Validación del formato.
+
+        if (!in_array($file['type'], $allowed_types)) 
+        {
+            echo "
+                <script>
+                    alert('
+                        Error en campo $key: 
+                            Tipo de archivo no permitido.
+                    ')
+                </script>";
+
+            continue;                                                   // Salta a la siguiente iteración del bucle.
+        }
+
+        $max_size = 2 * 1024 * 1024;                                    // Validación del tamaño del archivo (máximo 2 MB)
+
+        if ($file['size'] > $max_size)                                  // Validación del tamaño. 
+        {
+            echo "
+                <script>
+                    alert('
+                        Error en campo $key: 
+                            El archivo supera el tamaño permitido.
+                    ')
+                </script>";
+
+            continue;                                                   // Salta a la siguiente iteración del bucle.
+        }
+
+        $file_name = $file["name"];                                     // Obtenemos el nombre de cada fichero.
+
+        $data .= "$key: $directory$file_name\n";                        // Registramos la clave del campo y su ruta de almacenamiento.
+
+        move_uploaded_file(                                             // Movemos el fichero de la ruta temporal a la ruta de ./$directory/$file_name
+            $file["tmp_name"],                      
+            $directory . $file_name
+        );
+    }                              
+
+    $data .= "\n";
+
+    file_put_contents("formulario_recibido.txt", $data, FILE_APPEND);   // Almacenamos los datos recibidos en el fichero de destinado.
 
 ?>
