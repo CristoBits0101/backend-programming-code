f944c49d38bc69fb67a1ddff33a361a29c3807aa
CRPS <cristobits0101@gmail.com>
2023-11-13 08:56:06 +0000

    Refactorizando exam ut1 y ut2

--- PHP/MisProgramas7/exam-ut2/crea_formulario_generico.php
+++ PHP/MisProgramas7/exam-ut2/crea_formulario_generico.php
@@ -7,150 +7,75 @@
     </head>
     <body>
         <?php                          
-
-            $_route = "./estructura_formulario.cfg";                                // 1. Obtener la ruta del fichero que queremos manipular.
-
-            if (file_exists($_route))                                               // 2. Comprobar que el fichero existe en la ruta que nos han facilitado.
-
-                if (($_file = fopen( $_route, "r")) !== false)                      // 3. Comprobar si el fichero se puede abrir y manipular sin errores.
+            if (file_exists("./estructura_formulario.cfg") == true && ($_file = fopen("./estructura_formulario.cfg", "r")) !== false) 
+            {
+                while (($_line = fgets($_file)) !== false)
                 {
-                    while (($_line = fgets($_file)) !== false)                      // 4. Leemos las líneas del fichero mientras no contengan un valor falso.
+                    $_text = explode(';', $_line);
+
+                    foreach ($_text as $_value)
                     {
-                        $_text = explode(';', $_line);                              // 5. Dividimos la línea para organizar en posiciones.
-
-                        foreach ($_text as $_value)                                 // 6. Comprobamos los valores del array para saber el tipo de input.
+                        if (str_contains($_value, 'get') || str_contains($_value, 'post')) 
                         {
-                            if                                                          
-                            (
-                                str_contains($_value, 'get') ||
-                                str_contains($_value, 'post')
-                            )
-                            {
-                                $_action = trim($_text[0]);
-                                $_method = trim($_text[1]);
+                            $_action = trim($_text[0]); 
+                            $_method = trim($_text[1]);
+                            echo "<form action='$_action' method='$_method' enctype='multipart/form-data'>\n";
+                        }
+                        
+                        elseif (str_contains($_value, 'text') || str_contains($_value, 'password') || str_contains($_value, 'email') || str_contains($_value, 'file'))
+                        {
+                            $_field_data = trim($_text[0]);
+                            $_field_name = trim($_text[1]);
+                            $_field_type = trim($_text[2]);
+                            echo "$_field_data: </br> <input name='$_field_name' type='$_field_type'/> <br/><br/>";
+                        }
 
-                                echo "
-                                    <form 
-                                        action='$_action' 
-                                        method='$_method' 
-                                        enctype='multipart/form-data'
-                                    >
-                                \n";
-                            }
+                        elseif (str_contains($_value, 'checkbox'))
+                        {
+                            $_field_data = trim($_text[0]);
+                            $_field_name = trim($_text[1]);
+                            $_field_type = trim($_text[2]);
+
+                            // Convertimos el name de input en un array.
+                            $_field_name = $_field_name . '[]';
+
+                            $_value_checkbox = explode(',', $_text[3]);
                             
-                            elseif 
-                            (
-                                str_contains($_value, 'text')     ||
-                                str_contains($_value, 'password') ||
-                                str_contains($_value, 'email')    ||
-                                str_contains($_value, 'file')
-                            )
+                            echo $_field_data . '<br/><br/>';
+
+                            foreach($_value_checkbox as $_data)
                             {
-                                $_field_data = trim($_text[0]);
-                                $_field_name = trim($_text[1]);
-                                $_field_type = trim($_text[2]);
-
-                                echo "
-                                    $_field_data:
-
-                                    </br>
-
-                                    <input 
-                                        name='$_field_name'
-                                        type='$_field_type'  
-                                    />
-                                    
-                                    <br/>
-                                    <br/>";
+                                $_purified_data = trim($_data);
+                                echo "<input name='$_field_name' type='$_field_type' value='$_purified_data'/> $_data <br/><br/>";
                             }
+                        }
 
-                            elseif
-                            (
-                                str_contains($_value, 'checkbox')
-                            )
+                        elseif (str_contains($_value, 'radio'))
+                        {
+                            $_field_data = trim($_text[0]);
+                            $_field_name = trim($_text[1]);
+                            $_field_type = trim($_text[2]);
+
+                            $_value_checkbox = explode(',', $_text[3]);
+
+                            echo $_field_data . '<br/><br/>';
+
+                            foreach($_value_checkbox as $_data)
                             {
-                                $_field_data = trim($_text[0]);
-                                $_field_name = trim($_text[1]);
-                                $_field_type = trim($_text[2]);
-
-                                $_field_name = $_field_name . '[]';                 // Convertimos el name de input en un array.
-
-                                $_value_checkbox = explode(',', $_text[3]);
-
-                                echo $_field_data . '<br/><br/>';
-
-                                foreach($_value_checkbox as $_data)
-                                {
-                                    $_purified_data = trim($_data);
-
-                                    echo "
-                                        <input 
-                                            name='$_field_name'
-                                            type='$_field_type'
-                                            value='$_purified_data' 
-                                        />
-
-                                        $_data
-                                        
-                                        <br/>
-                                        <br/>";
-                                }
-                            }
-
-                            elseif
-                            (
-                                str_contains($_value, 'radio')
-                            )
-                            {
-                                $_field_data = trim($_text[0]);
-                                $_field_name = trim($_text[1]);
-                                $_field_type = trim($_text[2]);
-
-                                $_value_checkbox = explode(',', $_text[3]);
-
-                                echo $_field_data . '<br/><br/>';
-
-                                foreach($_value_checkbox as $_data)
-                                {
-                                    $_purified_data = trim($_data);
-
-                                    echo "
-                                        <input 
-                                            name='$_field_name'
-                                            type='$_field_type'
-                                            value='$_purified_data' 
-                                        />
-
-                                        $_data
-                                        
-                                        <br/>
-                                        <br/>";
-                                }
+                                $_purified_data = trim($_data);
+                                echo "<input name='$_field_name' type='$_field_type' value='$_purified_data'/> $_data <br/><br/>";
                             }
                         }
                     }
-
-                    echo "
-                        <input 
-                            type='submit' 
-                            value='Submit' 
-                        />";
-
-                    echo "</form>";
-
-                    fclose($_file);                                                 // Después de serializar el archivo lo cerramos.
                 }
+
+                echo "<input type='submit' value='Submit'/>";
+                echo "</form>";
                 
-                else                                                                // Indicar que el fichero no se puede abrir correctamente.
-                    echo "
-                        <script>
-                            alert('No se puedo leer el fichero.')
-                        </script>";
-            else                                                                    // Indicar que el fichero no existe en la ruta facilitada.
-                echo "
-                    <script>
-                        alert('El fichero no existe.')
-                    </script>";
+                fclose($_file);                                                   
+            }
+            
+            else echo "<script> alert('No se puedo leer el fichero.') </script>";
         ?>
     </body>
 </html>
