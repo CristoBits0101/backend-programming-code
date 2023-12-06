1c91df5651ee8178ea0ec09ad264284fc2f7cf63
CRPS <cristobits0101@gmail.com>
2023-12-06 16:38:40 +0000

    Refactorizando Java

---
+++ Java/E-S/Keyboard.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Keyboard 
+{
+    public static void main(String[] args) 
+    {
+        // 1. Crear un objeto Scanner para leer desde el teclado
+        Scanner scanner = new Scanner(System.in);
+
+        // 2. Solicitar al usuario que ingrese un entero
+        System.out.print("Por favor, ingresa un número entero: ");
+
+        // 3. Leer el entero ingresado por el usuario
+        int numeroEntero = scanner.nextInt();
+
+        // 4. Mostrar el número ingresado
+        System.out.println("Has ingresado: " + numeroEntero);
+
+        // 5. Cerrar el objeto Scanner para liberar recursos
+        scanner.close();
+    }
+}
