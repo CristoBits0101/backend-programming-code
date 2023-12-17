d2f993f6779d8ea385a41d3ac689abd616d8cb32
CRPS <cristobits0101@gmail.com>
2023-12-17 16:54:03 +0000

    Añadiendo anotaciones del curso de spring

--- Spring/01_Beans/1_Creation.java
+++ Spring/01_Beans/1_Creation.java
@@ -2,16 +2,20 @@
  *   1.   Crear el archivo de configuración : src/main/resources/beans.xml
  * 
  *   2.                bean id="calculator" : Cuando instancias un objeto de la clase Calculator hay que hacerlo mediante el identificador.
- *   3. bean class="com.example.Calculator" : Aquí va el path de la clase.
+ *   3.      class="com.example.Calculator" : Aquí va el path de la clase.
+ *   4.                   scope="singleton" : Crea un solo objeto para cada tipo de bean, modificar un atributo se modifica en todas las clases.
+ *   5.                   scope="prototype" : Crea nuevos objeto con sus propios atributos, modificar un atributo no afecta al resto de clases.
  * 
- *   4.                       Crea la clase : Calculator.java
+ *   6. Cuando el objeto recibe otro objeto :
  * 
- *   5.   Importar la dependencia 1 en Main : import org.springframework.context.ApplicationContext;
- *   6.   Importar la dependencia 2 en Main : import org.springframework.context.support.ClassPathXmlApplicationContext;
+ *      <bean id="gestorFacturas" class="GestorFacturas">
  * 
- *   7.               Importar el beans.xml : ApplicationContext context = ClassPathXmlApplicationContext("beans.xml");
- *   8.  Instanciar un objeto mediante bean : Calculator calculadora = (Calculator) context.getBean("Calculator");
+ *          -- name es el nombre del atributo de la clase que recibe el objeto.
+ *          -- ref es para indicar el identificador de la otra clase.
+ *          <constructor-arg name="calculadora" ref="calculadora"/>
  * 
- *   9.                      Usar el código : int result = calculadora.suma(2, 3); 
- *  10.                  Imprimir el código : System.out.println(result);
+ *          -- En este caso nombre es un atributo que recibe una String y no un objeto.
+ *          <constructor-arg name="nombre" value="Facturator 3000"/>
+ * 
+ *      </bean>
  */
