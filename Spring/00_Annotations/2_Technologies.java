198d019004e4cb362f2094c5ec3581f0f4b2b9c4
CRPS <cristobits0101@gmail.com>
2023-12-18 15:02:35 +0000

    Creando una api con Spring

--- Spring/00_Annotations/2_Technologies.java
+++ Spring/00_Annotations/2_Technologies.java
@@ -1,4 +1,6 @@
 /**
- *  Spring Data JPA : JPA -> Hibernate -> JDBC
- *      H2 Database : In-memory database for development testing.
+ *            Spring Web : MVC + Rest + Tomcat + JSON
+ *       Spring Data JPA : JPA -> Hibernate -> JDBC
+ *           H2 Database : In-memory database for development testing.
+ *  Spring Boot DevTools : Auto-refresh of changes, without reloading or build the app.
  */ 
