ea583e0bbecfc03703e4fb33b7b67727c9e92627
CRPS <cristobits0101@gmail.com>
2023-12-20 13:48:19 +0000

    Actualizando folder of English and Spring

--- Spring/00_Annotations/2_Technologies.java
+++
@@ -1,6 +0,0 @@
-/**
- *            Spring Web : MVC + Rest + Tomcat + JSON
- *       Spring Data JPA : JPA -> Hibernate -> JDBC
- *           H2 Database : In-memory database for development testing.
- *  Spring Boot DevTools : Auto-refresh of changes, without reloading or build the app.
- */ 
