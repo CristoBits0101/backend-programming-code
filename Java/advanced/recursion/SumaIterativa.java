607cfae18dad68f9a030d04e1a43b7dbb5282f99
Cristo RPS <cristo@gmail.com>
2024-06-29 21:35:07 +0100

    Añadiendo suma iterativa y recursiva por cola y cabeza

--- Java/advanced/recursion/SumaIterativa.java
+++ Java/advanced/recursion/SumaIterativa.java
@@ -1,24 +1,16 @@
 package advanced.recursion;
 
-public class RecursiveFunction {
+public class SumaIterativa {
 
     public static void main(String[] args) {
         System.out.println(sumaIterativa(5));
-        System.out.println();
-        System.out.println(sumaIterativa(5));
     }
 
-    // Ejemplo 1
     public static int sumaIterativa(int n) {
         int resultado = 0;
         for (int i = 0; i <= n; i++)
             resultado += i;
         return resultado;
-    }
-
-    // Ejemplo 2
-    public static int sumaRecursiva(int n) {
-        return (n == 0) ? 0 : n + sumaRecursiva(n - 1);
     }
 
 }
